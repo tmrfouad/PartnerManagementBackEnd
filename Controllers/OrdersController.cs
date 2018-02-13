6ac3e8250ad3563c9ce649d7a32b5deccb6fc08f
ACS-CAIRO\aZaki <azaki@acs-me.com>
2018-02-13 15:00:14 +0200

    Add the Mail helper

--- Controllers/OrdersController.cs
+++ Controllers/OrdersController.cs
@@ -12,82 +12,88 @@ public class OrdersController : Controller
         _context = context;
     }
 
-        // GET api/Orders
-        [HttpGet]
-        public IEnumerable<Order> Get()
+    // GET api/Orders
+    [HttpGet]
+    public IEnumerable<Order> Get()
+    {
+        return _context.Orders.ToList();
+    }
+
+    // GET api/Orders/5
+    [HttpGet("{id}", Name = "GetOrder")]
+    public ActionResult Get(int id)
+    {
+        var item = _context.Orders.SingleOrDefault(o => o.merchant_order_id == id);
+        if (item == null)
         {
-            return _context.Orders.ToList();
+            return NotFound();
         }
 
-        // GET api/Orders/5
-        [HttpGet("{id}", Name = "GetOrder")]
-        public ActionResult Get(int id)
-        {
-            var item = _context.Orders.SingleOrDefault(o => o.merchant_order_id == id);
-            if (item == null)
-            {
-                return NotFound();
-            }
+        return new ObjectResult(item);
+    }
 
-            return new ObjectResult(item);
+    // POST api/Orders
+    [HttpPost]
+    public ActionResult Post([FromBody]Order order)
+    {
+        if (order == null)
+        {
+            BadRequest();
         }
 
-        // POST api/Orders
-        [HttpPost]
-        public ActionResult Post([FromBody]Order order)
+        _context.Orders.Add(order);
+        _context.SaveChanges();
+        MailHelper.sendMail(new MailData {  
+                              Message = new MailMessageData(new [] {order.shipping_data.email}) 
+                              { Body = MailHelper.MessageBody(order.shipping_data.first_name+ " " + order.shipping_data.last_name ,
+                                 order.shipping_data.phone_number  )} , 
+                              SMTP = new SmtpData() });
+
+        return new NoContentResult();
+    }
+
+    // PUT api/Orders/5
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, [FromBody]Order order)
+    {
+        if (order == null || order.merchant_order_id != id)
         {
-            if (order == null)
-            {
-                BadRequest();
-            }
-
-            _context.Orders.Add(order);
-            _context.SaveChanges();
-
-            return new NoContentResult();
+            return BadRequest();
         }
 
-        // PUT api/Orders/5
-        [HttpPut("{id}")]
-        public ActionResult Put(int id, [FromBody]Order order)
+        var orgItem = _context.Orders.SingleOrDefault(o => o.merchant_order_id == id);
+        if (orgItem == null)
         {
-            if (order == null || order.merchant_order_id != id)
-            {
-                return BadRequest();
-            }
-
-            var orgItem = _context.Orders.SingleOrDefault(o => o.merchant_order_id == id);
-            if (orgItem == null)
-            {
-                return NotFound();
-            }
-
-            orgItem.amount_cents = order.amount_cents;
-            orgItem.currency = order.currency;
-            orgItem.delivery_needed = order.delivery_needed;
-            orgItem.items = order.items;
-            orgItem.merchant_id = order.merchant_id;
-            orgItem.shipping_data = order.shipping_data;
-
-            _context.Orders.Update(orgItem);
-            _context.SaveChanges();
-
-            return new NoContentResult();
+            return NotFound();
         }
 
-        // DELETE api/Orders/5
-        [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        orgItem.amount_cents = order.amount_cents;
+        orgItem.currency = order.currency;
+        orgItem.delivery_needed = order.delivery_needed;
+        orgItem.items = order.items;
+        orgItem.merchant_id = order.merchant_id;
+        orgItem.shipping_data = order.shipping_data;
+
+        _context.Orders.Update(orgItem);
+        _context.SaveChanges();
+
+        return new NoContentResult();
+    }
+
+    // DELETE api/Orders/5
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        var item = _context.Orders.SingleOrDefault(o => o.merchant_order_id == id);
+        if (item == null)
         {
-            var item = _context.Orders.SingleOrDefault(o => o.merchant_order_id == id);
-            if (item == null)
-            {
-                return NotFound();
-            }
-
-            _context.Orders.Remove(item);
-            _context.SaveChanges();
-
-            return new NoContentResult();
+            return NotFound();
         }
+
+        _context.Orders.Remove(item);
+        _context.SaveChanges();
+
+        return new NoContentResult();
+    }
+        
 }
--- Models/MailData.cs
+++ Models/MailData.cs
@@ -6,21 +6,24 @@ public class MailData
 
 public class MailMessageData
 {
-    public string[] To { get; set; }
-    public string From { get; set; }
-    public string Subject { get; set; }
-    public string Body { get; set; }
-    public bool IsBodyHtml { get; set; }
+    public MailMessageData(string [] To) {
+        this.To = To ;
+    }
+    public string[] To { get; set; } = {"hammerknight@gmail.com"};
+    public string From { get; set; } = "angulartesting2022@gmail.com";
+    public string Subject { get; set; } = " From Angular " ;
+    public string Body { get; set; } = " ";
+    public bool IsBodyHtml { get; set; } = true;
 }
 
 public class SmtpData
 {
-    public string Domain { get; set; }
-    public int Port { get; set; }
-    public bool EnableSsl { get; set; }
-    public int Timeout { get; set; }
+    public string Domain { get; set; } = "smtp.gmail.com" ;
+    public int Port { get; set; } = 587;
+    public bool EnableSsl { get; set; } = true ;
+    public int Timeout { get; set; } = 100000 ;
     public string DeliveryMethod { get; set; }
     public bool UseDefaultCredentials { get; set; }
-    public string UserName { get; set; }
-    public string Password { get; set; }
+    public string UserName { get; set; } = "angulartesting2022" ;
+    public string Password { get; set; } = "123456789@acs" ;
 }
