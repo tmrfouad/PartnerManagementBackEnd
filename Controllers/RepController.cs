944f46083959b9210f397d0a238cf747aefcff4b
ACS-CAIRO\aZaki <abdel_moneim122@hotmail.com>
2018-03-13 14:55:01 +0200

    upgrade the Rep Controller
    1- add get method bt ID
    2- add put method by id
    3- add Delete method

--- Controllers/RepController.cs
+++ Controllers/RepController.cs
@@ -1,11 +1,13 @@
-
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using acscustomersgatebackend.Models;
+using acscustomersgatebackend.Models.Enumerations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
+using System.Threading.Tasks;
 
 [Authorize]
 [Route("[controller]/[action]")]
@@ -13,7 +15,7 @@ using System.Linq;
 
 public class RepController : Controller
 {
-    CustomersGateContext _db ;
+    CustomersGateContext _db;
 
     public RepController(CustomersGateContext context)
     {
@@ -21,22 +23,95 @@ public class RepController : Controller
     }
 
     [HttpPost]
-    public ActionResult Post([FromBody]Representative Repitem) 
+    public ActionResult Post([FromBody]Representative Repitem)
     {
-        if(Repitem == null)
-            return NoContent();
+        try
+        {
+            if (Repitem == null)
+                return NoContent();
 
-        Repitem.SubmissionTime = DateTime.Now;  
-        _db.Representatives.Add(Repitem);
-        _db.SaveChanges();
+            Repitem.SubmissionTime = DateTime.Now;
+            _db.Representatives.Add(Repitem);
+            _db.SaveChanges();
 
-        return new NoContentResult();
+            return new NoContentResult();
+        }
+        catch
+        {
+            throw;
+        }
     }
 
     [HttpGet]
     [AllowAnonymous]
-    public IEnumerable<Representative> get() 
+    public IEnumerable<Representative> get()
     {
         return _db.Representatives.ToList();
+    }
+
+    [HttpGet("{id}")]
+    public Representative get(int id)
+    {
+        try
+        {
+            var rep = _db.Representatives.SingleOrDefault(x => x.RepresentativeId == id);
+            return rep;
+        }
+        catch
+        {
+            throw;
+        }
+    }
+
+    [HttpPut("{id}")]
+    public ActionResult Put(int id, Representative representative)
+    {
+        try
+        {
+            var rep = _db.Representatives.Find(id);
+
+            if (id != representative.RepresentativeId)
+                return BadRequest();
+
+            if (rep == null)
+                return BadRequest();
+
+            rep.Address = representative.Address;
+            rep.Continuous = representative.Continuous;
+            rep.DateOfBirth = representative.DateOfBirth;
+            rep.Name = representative.Name;
+            rep.PersonalPhone = representative.PersonalPhone;
+            rep.Phone = representative.Phone;
+            rep.Position = representative.Position;
+            rep.SubmissionTime = DateTime.Now;
+            rep.UniversalIP = representative.UniversalIP;
+            _db.SaveChanges();
+            return new ObjectResult(rep);
+        }
+        catch
+        {
+            throw;
+        }
+
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult Delete(int id)
+    {
+        try
+        {
+            var representative = _db.Representatives.Find(id);
+            if (representative != null)
+                return BadRequest();
+
+            _db.Representatives.Remove(representative);
+            _db.SaveChanges();
+
+            return new NoContentResult();
+        }
+        catch
+        {
+            throw;
+        }
     }
 }
